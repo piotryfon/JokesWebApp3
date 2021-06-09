79935ed4dc854ca60e60f5c8d957a40bc43d8ed5
piotryfon <pit.pit@wp.pl>
2021-06-09 18:29:08 +0200

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using JokesWebApp3.Models;
+
+namespace JokesWebApp3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesWebApp3.Models.Joke> Joke { get; set; }
+    }
+}
